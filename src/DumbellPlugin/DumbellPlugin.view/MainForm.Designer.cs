9711c13ea1df94f64d2bdd259e4f95b3090cfee2
andre <andre@LAPTOP-26589CQ6>
2023-10-27 15:43:23 +0700

    Создал лэйблы и кнопки и текстбоксы

--- src/DumbellPlugin/DumbellPlugin.view/MainForm.Designer.cs
+++ src/DumbellPlugin/DumbellPlugin.view/MainForm.Designer.cs
@@ -28,12 +28,321 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            label7 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            button1 = new Button();
+            label17 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(117, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Длина рукоятки:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(117, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Диаметр рукоятки:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(117, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Ширина крепления:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(117, 233);
+            label4.Name = "label4";
+            label4.Size = new Size(152, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Внешний диаметр дисков:";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(117, 203);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Количество дисков:";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(117, 173);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 15);
+            label6.TabIndex = 3;
+            label6.Text = "Диаметр крепления:";
+            label6.Click += label6_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(117, 293);
+            label8.Name = "label8";
+            label8.Size = new Size(92, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Ширина диска: ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(117, 263);
+            label9.Name = "label9";
+            label9.Size = new Size(166, 15);
+            label9.TabIndex = 6;
+            label9.Text = "Внутренний диаметр дисков:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(290, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 8;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(290, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(290, 140);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(290, 170);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(290, 200);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(290, 230);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(290, 260);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(290, 290);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(405, 83);
+            label7.Name = "label7";
+            label7.Size = new Size(95, 15);
+            label7.TabIndex = 16;
+            label7.Text = "от 320 до 400мм";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(405, 113);
+            label10.Name = "label10";
+            label10.Size = new Size(83, 15);
+            label10.TabIndex = 17;
+            label10.Text = "от 20 до 40мм";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(405, 143);
+            label11.Name = "label11";
+            label11.Size = new Size(37, 15);
+            label11.TabIndex = 18;
+            label11.Text = "10мм";
+            label11.Click += label11_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(405, 173);
+            label12.Name = "label12";
+            label12.Size = new Size(83, 15);
+            label12.TabIndex = 19;
+            label12.Text = "от 40 до 80мм";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(405, 263);
+            label13.Name = "label13";
+            label13.Size = new Size(92, 15);
+            label13.TabIndex = 20;
+            label13.Text = "от 20.1 до 44мм";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(405, 203);
+            label14.Name = "label14";
+            label14.Size = new Size(53, 15);
+            label14.TabIndex = 20;
+            label14.Text = "от 1 до 5";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(405, 233);
+            label15.Name = "label15";
+            label15.Size = new Size(95, 15);
+            label15.TabIndex = 21;
+            label15.Text = "от 100 до 150мм";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(405, 293);
+            label16.Name = "label16";
+            label16.Size = new Size(83, 15);
+            label16.TabIndex = 22;
+            label16.Text = "от 10 до 50мм";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(400, 328);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 23);
+            button1.TabIndex = 23;
+            button1.Text = "Построить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(126, 332);
+            label17.Name = "label17";
+            label17.Size = new Size(0, 15);
+            label17.TabIndex = 24;
+            label17.Click += label17_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(633, 450);
+            Controls.Add(label17);
+            Controls.Add(button1);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label7);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "MainForm";
+            Text = "Гантель";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label label7;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Button button1;
+        private Label label17;
     }
 }
--- src/DumbellPlugin/DumbellPlugin.view/MainForm.cs
+++ src/DumbellPlugin/DumbellPlugin.view/MainForm.cs
@@ -6,5 +6,45 @@ namespace DumbellPlugin.view
         {
             InitializeComponent();
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int parsedValue;
+
+            if (!int.TryParse(textBox1.Text, out parsedValue) || parsedValue > 400 || parsedValue < 320)
+            {
+                textBox1.BackColor = Color.Pink;
+                label17.Text = "Ошибка! Длина рукоятки.";
+                label17.BackColor = Color.Pink;
+            }
+            else
+            {
+                textBox1.BackColor = SystemColors.Window; // Восстановить стандартный цвет фона
+                label17.Text = ""; // Очистить текст в label17
+            }
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

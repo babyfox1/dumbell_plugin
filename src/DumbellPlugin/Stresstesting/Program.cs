26713d95efb9bc4ee38d7091094c2ef7187115ca
andre <andre@LAPTOP-26589CQ6>
2023-12-28 13:53:33 +0700

    Написал StressTesting

---
+++ src/DumbellPlugin/Stresstesting/Program.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace StressTesting
+{
+    using System.Diagnostics;
+    using DumbellPlugin.Model;
+    using DumbellPlugin.Wrapper;
+    using NickStrupat;
+
+    /// <summary>
+    /// Класс для нагрузочного тестирования.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка входа.
+        /// </summary>
+        /// <param name="args">Аргументы.</param>
+        private static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var parameters = new Parameters();
+            var streamWriter = new StreamWriter("log.txt", true);
+            var count = 0;
+            while (true)
+            {
+                const double gigabyteInByte = 0.000000000931322574615478515625;
+                var builder = new Builder();
+                builder.BuildDetail(parameters);
+                var computerInfo = new ComputerInfo();
+                var usedMemory = (computerInfo.TotalPhysicalMemory
+                                  - computerInfo.AvailablePhysicalMemory)
+                                 * gigabyteInByte;
+                streamWriter.WriteLine(
+                    $"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}");
+                streamWriter.Flush();
+            }
+        }
+    }
+}
